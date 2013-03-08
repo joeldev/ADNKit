d0909955de5d70f83b1cb844204b25d51fe6b232
Joel Levin <joellevin.email@gmail.com>
2013-03-08 00:15:41 -0800

    Channel API integration

--- ADNKit/API Calls/ADNClient+ADNChannel.h
+++ ADNKit/API Calls/ADNClient+ADNChannel.h
@@ -9,8 +9,32 @@
 #import "ADNClient.h"
 
 
+@class ADNChannel, ADNACL, ADNUser;
+
 @interface ADNClient (ADNChannel)
 
+- (void)fetchCurrentUserSubscribedChannelsWithCompletion:(ADNClientCompletionBlock)completionHandler;
+- (void)fetchCurrentUserCreatedChannelsWithCompletion:(ADNClientCompletionBlock)completionHandler;
+- (void)fetchUnreadPMChannelsCountWithCompletion:(ADNClientCompletionBlock)completionHandler;
 
+- (void)fetchChannelWithID:(NSString *)channelID completion:(ADNClientCompletionBlock)completionHandler;
+- (void)fetchChannelsWithIDs:(NSArray *)channelIDs completion:(ADNClientCompletionBlock)completionHandler;
+
+- (void)fetchUsersSubscribedToChannel:(ADNChannel *)channel completion:(ADNClientCompletionBlock)completionHandler;
+- (void)fetchUsersSubscribedToChannelWithID:(NSString *)channelID completion:(ADNClientCompletionBlock)completionHandler;
+- (void)fetchUserIDsSubscribedToChannel:(ADNChannel *)channel completion:(ADNClientCompletionBlock)completionHandler;
+- (void)fetchUserIDsSubscribedToChannelWithID:(NSString *)channelID completion:(ADNClientCompletionBlock)completionHandler;
+- (void)fetchUserIDsSubscribedToChannelIDs:(NSArray *)channelIDs completion:(ADNClientCompletionBlock)completionHandler;
+
+- (void)createChannel:(ADNChannel *)channel completion:(ADNClientCompletionBlock)completionHandler;
+- (void)createChannelWithType:(NSString *)type readers:(ADNACL *)readersACL writers:(ADNACL *)writersACL completion:(ADNClientCompletionBlock)completionHandler;
+
+- (void)updateChannel:(ADNChannel *)channel completion:(ADNClientCompletionBlock)completionHandler;
+- (void)updateChannelWithID:(NSString *)channelID readers:(ADNACL *)readersACL writers:(ADNACL *)writersACL completion:(ADNClientCompletionBlock)completionHandler;
+
+- (void)subscribeToChannel:(ADNChannel *)channel completion:(ADNClientCompletionBlock)completionHandler;
+- (void)subscribeToChannelWithID:(NSString *)channelID completion:(ADNClientCompletionBlock)completionHandler;
+- (void)unsubscribeToChannel:(ADNChannel *)channel completion:(ADNClientCompletionBlock)completionHandler;
+- (void)unsubscribeToChannelWithID:(NSString *)channelID completion:(ADNClientCompletionBlock)completionHandler;
 
 @end
