11453a6d0f011e0c8552023059e1b7145bf36512
Joel Levin <joellevin.email@gmail.com>
2013-03-09 09:46:39 -0800

    Added ANKFilter API integration skeleton

---
+++ ADNKit/API Calls/ANKClient+ANKFilter.h
@@ -0,0 +1,16 @@
+//
+//  ANKClient+ANKFilter.h
+//  ADNKit
+//
+//  Created by Joel Levin on 3/8/13.
+//  Copyright (c) 2013 Afterwork Studios. All rights reserved.
+//
+
+#import "ANKClient+ANKHandlerBlocks.h"
+
+
+@interface ANKClient (ANKFilter)
+
+
+
+@end
---
+++ ADNKit/API Calls/ANKClient+ANKFilter.m
@@ -0,0 +1,16 @@
+//
+//  ANKClient+ANKFilter.m
+//  ADNKit
+//
+//  Created by Joel Levin on 3/8/13.
+//  Copyright (c) 2013 Afterwork Studios. All rights reserved.
+//
+
+#import "ANKClient+ANKFilter.h"
+
+
+@implementation ANKClient (ANKFilter)
+
+
+
+@end
