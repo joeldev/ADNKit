0f931317f4d169a3f5dcd4359fc78b84824a7a07
Joel Levin <joellevin.email@gmail.com>
2013-03-18 11:43:58 -0700

    Tweaked build script to generate universal iOS binaries and rebuilt

--- Scripts/make_release.sh
+++ Scripts/make_release.sh
@@ -6,9 +6,12 @@ cd ADNKit && find . -iname "*.h" -exec cp -v "{}" "../Release/ADNKit/" \;
 cd ../External/AFNetworking/AFNetworking && find . -iname "*.h" -exec cp -v "{}" "../../../Release/ADNKit/" \;
 cd ../../../
 
-xcodebuild clean build -target ADNKit-iOS
+xcodebuild clean build -target ADNKit-iOS -sdk iphoneos
+xcodebuild build -target ADNKit-iOS -sdk iphonesimulator
+
+lipo -create -output Release/libADNKit-iOS.a build/Release-iphoneos/libADNKit-iOS.a build/Release-iphonesimulator/libADNKit-iOS.a
+
 xcodebuild clean build -target ADNKit-OSX
 
 cp -R build/Release/* Release/
-cp build/Release-iphoneos/*.a Release/
 rm -rf build
