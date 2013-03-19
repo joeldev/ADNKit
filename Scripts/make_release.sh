a8489ce7b4799f330b024c1f0025734f1e9d74a0
Joel Levin <joellevin.email@gmail.com>
2013-03-19 00:04:05 -0700

    PHEW, finally got this working

--- Scripts/make_release.sh
+++ Scripts/make_release.sh
@@ -3,15 +3,20 @@
 rm -rf Release
 mkdir -p Release/ADNKit
 cd ADNKit && find . -iname "*.h" -exec cp -v "{}" "../Release/ADNKit/" \;
-cd ../External/AFNetworking && find . -iname "*.h" -exec cp -v "{}" "../../Release/ADNKit/" \;
-cd ../../
+cd ../Pods/AFNetworking/AFNetworking && find . -iname "*.h" -exec cp -v "{}" "../../../Release/ADNKit/" \;
+cd ../../../
 
-xcodebuild clean build -target ADNKit-iOS -sdk iphoneos
-xcodebuild build -target ADNKit-iOS -sdk iphonesimulator
+OUTPUT_DIR="`echo $(pwd)/build/`"
+
+xcodebuild clean -workspace ADNKit.xcworkspace -scheme ADNKit-OSX -configuration Release SYMROOT=${OUTPUT_DIR}
+xcodebuild clean -workspace ADNKit.xcworkspace -scheme ADNKit-iOS -configuration Release SYMROOT=${OUTPUT_DIR}
+
+xcodebuild build -workspace ADNKit.xcworkspace build -scheme ADNKit-iOS -sdk iphoneos -configuration Release SYMROOT=${OUTPUT_DIR}
+xcodebuild build -workspace ADNKit.xcworkspace build -scheme ADNKit-iOS -sdk iphonesimulator -configuration Release SYMROOT=${OUTPUT_DIR}
+xcodebuild build -workspace ADNKit.xcworkspace build -scheme ADNKit-OSX -configuration Release SYMROOT=${OUTPUT_DIR}
 
 lipo -create -output Release/libADNKit-iOS.a build/Release-iphoneos/libADNKit-iOS.a build/Release-iphonesimulator/libADNKit-iOS.a
 
-xcodebuild clean build -target ADNKit-OSX
-
 cp -R build/Release/* Release/
+rm Release/libPods-ADNKit-OSX.a
 rm -rf build
