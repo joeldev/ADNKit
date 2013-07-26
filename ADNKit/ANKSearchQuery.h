6f93e7d5b80d0d8df9045b677c5c70502d109dfd
Kolin Krewinkel <kolin@kolinkrewinkel.com>
2013-07-25 23:20:14 -0700

    Adds general and search query properties.

--- ADNKit/ANKSearchQuery.h
+++ ADNKit/ANKSearchQuery.h
@@ -6,6 +6,32 @@
 //  Copyright (c) 2013 Afterwork Studios. All rights reserved.
 //
 
+typedef enum {
+    ANKSearchQueryIndexTypeComplete
+} ANKSearchQueryIndexType;
+
+typedef enum {
+    ANKSearchQueryOrderTypeID,
+    ANKSearchQueryOrderTypeScore
+} ANKSearchQueryOrderType;
+
 @interface ANKSearchQuery : NSObject
+
+#pragma mark - 
+#pragma mark General Parameters
+
+@property (nonatomic) ANKSearchQueryIndexType indexType;
+@property (nonatomic) ANKSearchQueryOrderType orderType;
+
+#pragma mark -
+#pragma mark Search Query Parameters
+
+@property (nonatomic, copy) NSString *query;
+@property (nonatomic, copy) NSString *text;
+@property (nonatomic, copy) NSArray *hashtags;
+@property (nonatomic, copy) NSArray *links;
+@property (nonatomic, copy) NSArray *linkDomains;
+@property (nonatomic, copy) NSArray *mentions;
+@property (nonatomic, copy) NSArray *leadingMentions;
 
 @end
